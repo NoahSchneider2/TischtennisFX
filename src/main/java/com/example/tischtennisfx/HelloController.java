20d101e64d9fafe789410656fbf477d82aa8cebc
Lucas Breiner <l.breiner@imi.de>
2022-10-25 18:47:04 +0200

    WIP:
    * Namen wurden angepasst für bessere Lesbarkeit

--- src/main/java/com/example/tischtennisfx/HelloController.java
+++ src/main/java/com/example/tischtennisfx/HelloController.java
@@ -4,96 +4,122 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import java.util.List; // Package for the list
+
 import java.util.ArrayList; // Package for the ArrayList
 import javafx.collections.ObservableList; // Package for the ObservableList
-import javafx.collections.ListChangeListener; // Package for the listener for lists
 import javafx.collections.FXCollections;
-
-import java.util.ArrayList;
 
 public class HelloController {
 
-    private ArrayList<String> t1NList = new ArrayList<>();
-    private ArrayList<String> t2NList = new ArrayList<>();
+    private final ArrayList<Participant> participantsOfTeam1 = new ArrayList<Participant>();
+    private final ArrayList<Participant> participantsOfTeam2 = new ArrayList<Participant>();
 
-    private ArrayList<String> d1t1List = new ArrayList<>();
-    private ArrayList<String> d1t2List = new ArrayList<>();
-    private String t1Name;
-    private String t2Name;
+    private Team team1;
+    private Team team2;
 
+    private final ArrayList<Match> matches = new ArrayList<Match>();
 
-    @FXML
-    private TextField T1S1N;
 
     @FXML
-    private TextField T1S2N;
+    private TextField nameOfParticipant1;
 
     @FXML
-    private TextField T1S3N;
+    private TextField nameOfParticipant2;
 
     @FXML
-    private TextField T1S4N;
+    private TextField nameOfParticipant3;
 
     @FXML
-    private TextField T2S1N;
+    private TextField nameOfParticipant4;
 
     @FXML
-    private TextField T2S2N;
+    private TextField nameOfParticipant5;
 
     @FXML
-    private TextField T2S3N;
+    private TextField nameOfParticipant6;
 
     @FXML
-    private TextField T2S4N;
+    private TextField nameOfParticipant7;
 
     @FXML
-    private TextField t1N;
+    private TextField nameOfParticipant8;
 
     @FXML
-    private ChoiceBox<?> t1ds1;
+    private TextField nameOfTeam1;
+    @FXML
+    private TextField nameOfTeam2;
+    //TODO: Oof, gibts eine Möglichkeit die Felder zu gruppieren? @Noah
+    @FXML
+    private ChoiceBox<Participant> t1ds1;
 
     @FXML
-    private ChoiceBox<?> t1ds2;
+    private ChoiceBox<Participant> t1ds2;
 
     @FXML
-    private ChoiceBox<?> t2ds1;
-
+    private ChoiceBox<Participant> t2ds1;
     @FXML
-    private TextField t2N;
-
-    @FXML
-    private ChoiceBox<?> t2ds2;
+    private ChoiceBox<Participant> t2ds2;
 
     @FXML
     void doppelButton(ActionEvent event) {
-        t1Name = t1N.getText();
-        t2Name = t2N.getText();
+        team1 = new Team(nameOfTeam1.getText());
+        team2 = new Team(nameOfTeam2.getText());
 
-        t1NList.add(T1S1N.getText());
-        t1NList.add(T1S2N.getText());
-        t1NList.add(T1S3N.getText());
-        t1NList.add(T1S4N.getText());
+        participantsOfTeam1.add(new Participant(nameOfParticipant1.getText(), team1));
+        participantsOfTeam1.add(new Participant(nameOfParticipant2.getText(), team1));
+        participantsOfTeam1.add(new Participant(nameOfParticipant3.getText(), team1));
+        participantsOfTeam1.add(new Participant(nameOfParticipant4.getText(), team1));
 
-        t2NList.add(T2S1N.getText());
-        t2NList.add(T2S2N.getText());
-        t2NList.add(T2S3N.getText());
-        t2NList.add(T2S4N.getText());
+        participantsOfTeam2.add(new Participant(nameOfParticipant5.getText(), team2));
+        participantsOfTeam2.add(new Participant(nameOfParticipant6.getText(), team2));
+        participantsOfTeam2.add(new Participant(nameOfParticipant7.getText(), team2));
+        participantsOfTeam2.add(new Participant(nameOfParticipant8.getText(), team2));
+        // TODO: Zeile 68-76 mit einer Schleife irgendwie cooler machen?
 
-        ObservableList t1nOb = FXCollections.observableList(t1NList);
-        ObservableList t2nOb = FXCollections.observableList(t2NList);
+        ObservableList<Participant> t1nOb = FXCollections.observableList(participantsOfTeam1);
+        ObservableList<Participant> t2nOb = FXCollections.observableList(participantsOfTeam2);
         t1ds1.setItems(t1nOb);
-        t1ds1.setItems(t1nOb);
-        t1ds2.setItems(t2nOb);
-        t1ds2.setItems(t2nOb);
+        t1ds2.setItems(t1nOb);
+        t2ds1.setItems(t2nOb);
+        t2ds2.setItems(t2nOb);
     }
 
     @FXML
     void tunierstartButton(ActionEvent event) {
-        d1t1List.add(t1ds1.getValue().toString());
-        d1t1List.add(t1ds2.getValue().toString());
+        t1ds1.getValue().setDoppel(1);
+        t1ds2.getValue().setDoppel(1);
+        t1ds1.getValue().setDoppel(1);
+        t1ds2.getValue().setDoppel(1);
+        setDoppelForRemainingParticipants(participantsOfTeam1);
+        setDoppelForRemainingParticipants(participantsOfTeam2);
+        //TODO: Funktion die alle Matches erstellt.
+    }
 
-        d1t2List.add(t2ds1.getValue().toString());
-        d1t2List.add(t2ds2.getValue().toString());
+    private void setDoppelForRemainingParticipants(ArrayList<Participant> participants) {
+        for (Participant participant :
+                participants) {
+            if (participant.getDoppel() != 1) {
+                participant.setDoppel(2);
+            }
+        }
+    }
+    private void createMatches() {
+
+        new Match()
+    }
+
+    private Match createDoppelMatches() {
+        ArrayList<Participant> doppel1Participants = new ArrayList<Participant>();;
+        ArrayList<Participant> doppel2Participants = new ArrayList<Participant>();;
+
+        for (Participant participant :
+                participantsOfTeam1) {
+            if(participant.getDoppel() == 1) {
+                doppel1Participants.add(participant);
+                continue;
+            }
+            doppel2Participants.add(participant);
+        }
+        new Match(doppel1Participants, )
     }
 }
