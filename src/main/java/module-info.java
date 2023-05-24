29ae6f5d7aebff27cd68600117b96bd6b3146367
Lucas Breiner <l.breiner@imi.de>
2023-05-24 16:26:44 +0200

    adjust directory structure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,8 @@ module com.example.tischtennisfx {
 
     opens com.example.tischtennisfx to javafx.fxml;
     exports com.example.tischtennisfx;
+    exports com.example.tischtennisfx.models;
+    opens com.example.tischtennisfx.models to javafx.fxml;
+    exports com.example.tischtennisfx.controller;
+    opens com.example.tischtennisfx.controller to javafx.fxml;
 }
